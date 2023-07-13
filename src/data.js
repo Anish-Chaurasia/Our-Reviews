f6712080bd2e5341069b68bd53bedf409b735cbf
Anish Chaurasia <anishraut2111@gmail.com>
2023-07-13 20:00:22 +0530

    review

---
+++ src/data.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const arr = [{
+    image: "./images/user4.jpg",
+    profession: "Fullstack Developer",
+    name: "Anish Chaurasia ",
+    text: "A full-stack developer is a developer or engineer who can build both the front end and the back end of a website."
+},
+
+
+{
+    image: "./images/user 3.png",
+    profession: "Backend Developer",
+    name: "Rohan Sharma",
+    text: " backend Developer is responsible for designing, building, and maintaining the server side of web applications. "
+},
+
+{
+    image: "./images/user2.png",
+    profession: "System Engineer",
+    name: "Akhil Rawat",
+    text: "Systems engineering is an interdisciplinary field of engineering and engineering management that focuses on how to design, integrate, and manage complex systems over their life cycles."
+},
+
+
+{
+    image: "./images/user1.png",
+    profession: "UI/UX Developer",
+    name: "Sumit Thakur",
+    text: "A UI/UX designer's job is to create user-friendly interfaces that enable users to understand how to use complex technical products."
+}];
+
+export default arr;
+
